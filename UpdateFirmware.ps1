537ddd03289013f82454f1d3908c8e7255501767
Paul Grevink <paul.grevink@gmail.com>
2018-06-19 20:00:26 +0200

    Create UpdateFirmware.ps1

---
+++ UpdateFirmware.ps1
@@ -0,0 +1,19 @@
+$VMhostName = "esx01"
+$VMhostUser = "root"
+$VMhostPass = "pw"
+
+$HostInfo = Get-VMHost -Name $VMhostName
+
+Get-VMHost -Name $VMhostName | Foreach {Start-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"} )}
+
+if ($HostInfo.Model -eq "ProLiant DL380 Gen9" ) {
+    Write-Host "This is a ProLiant DL380 Gen9"
+    & "C:\Program Files (x86)\PuTTY\pscp.exe" -l $VMhostUser -pw $VMhostPass -r DL380Gen9\ ${VMhostName}:/tmp
+    & "C:\Program Files (x86)\PuTTY\plink.exe" $VMhostUser@$VMhostName -pw $VMhostPass -m .\firmware.plink
+}
+ 
+if ($HostInfo.Model -eq "ProLiant DL580 Gen9" ) {
+    Write-Host "This is a ProLiant DL580 Gen9"
+    & "C:\Program Files (x86)\PuTTY\pscp.exe" -l $VMhostUser -pw $VMhostPass -r DL580Gen9\ ${VMhostName}:/tmp
+    & "C:\Program Files (x86)\PuTTY\plink.exe" $VMhostUser@$VMhostName -pw $VMhostPass -m .\firmware.plink
+}
